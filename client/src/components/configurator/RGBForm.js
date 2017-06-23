537bf0acf3be312f6a5b46b9910883ccb77a98ba
Kyle Landell <kyle.landell@gmail.com>
2017-06-22 21:00:25 -0400

    Don't allow incorrect rgb values

--- client/src/components/configurator/RGBForm.js
+++ client/src/components/configurator/RGBForm.js
@@ -9,22 +9,52 @@ export default class RGBForm extends React.Component {
         onChange: PropTypes.func.isRequired,
     }
 
+    onRGBChange(e) {
+        const value = parseInt(e.currentTarget.value, 10);
+        const { onChange } = this.props;
+
+        if (!value || (value > 0 && value <= 255)) {
+            onChange(e);
+        }
+    }
+
     render() {
-        const { red, green, blue, onChange } = this.props;
+        const { red, green, blue } = this.props;
 
         return (
             <form className="color-form">
                 <div>
                     <label htmlFor="red">R</label>
-                    <input type="number" name="red" value={red} onChange={onChange} min="0" max="255" />
+                    <input
+                        type="number"
+                        name="red"
+                        value={red}
+                        onChange={e => this.onRGBChange(e)}
+                        min="0"
+                        max="255"
+                    />
                 </div>
                 <div>
                     <label htmlFor="green">G</label>
-                    <input type="number" name="green" value={green} onChange={onChange} min="0" max="255" />
+                    <input
+                        type="number"
+                        name="green"
+                        value={green}
+                        onChange={e => this.onRGBChange(e)}
+                        min="0"
+                        max="255"
+                    />
                 </div>
                 <div>
                     <label htmlFor="blue">B</label>
-                    <input type="number" name="blue" value={blue} onChange={onChange} min="0" max="255" />
+                    <input
+                        type="number"
+                        name="blue"
+                        value={blue}
+                        onChange={e => this.onRGBChange(e)}
+                        min="0"
+                        max="255"
+                    />
                 </div>
             </form>
         );
